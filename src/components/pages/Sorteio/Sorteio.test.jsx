0bdd9e7cf6a886c68c0f52b7ae9b2981646b860c
bernardo013 <japatvs1@gmail.com>
2026-02-13 20:27:25 -0300

    implementação sorteio

--- src/components/pages/Sorteio/Sorteio.test.jsx
+++ src/components/pages/Sorteio/Sorteio.test.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
 import Sorteio from './Sorteio'
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { listaParticipantesState } from "../../../state/atom";
+import useResultadoSorteio from "../../../state/hook/useResultadoSorteio";
 
 
 
-describe('a pagina de sorteio', () => {
+describe('na pagina de sorteio', () => {
     test('todos os participantes podem exibir o seu amigo secreto', () => {
         const participantes = ['bernardo', 'pedro', 'julia']
+        const resultado = new Map([
+            ['bernardo', 'pedro']
+            ['julia', 'bernardo']
+            ['pedro', 'julia']
+        ])
         
         render(
             <RecoilRoot  initializeState={({ set }) => {
@@ -21,4 +27,25 @@ describe('a pagina de sorteio', () => {
         expect(opcoes).toHaveLength(participantes.length)
 
 })
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(
+            <RecoilRoot initializeState={ ({ set }) => {
+                set(useResultadoSorteio, resultado)
+            }}>
+                <Sorteio/>
+            </RecoilRoot>
+
+        )
+            const select = screen.getByPlaceholderText('selecione o seu nome').ATTRIBUTE_NODE
+            fireEvent.change(select, {
+                target: {
+                    value: participantes
+                }
+            })
+            const botao = screen.getByRole('button')
+            fireEvent.click(botao)
+
+            const amigoSecreto = screen.getByRole('alert')
+            expect(amigoSecreto).toBeInTheDocument()
+    })
 })
--- src/components/pages/Sorteio/Sorteio.tsx
+++ src/components/pages/Sorteio/Sorteio.tsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useListaParticipantes } from '../../../state/hook/useListaParticipantes'
 import Header from '../../Header/Header'
 import style from './Sorteio.module.scss'
+import useResultadoSorteio from '../../../state/hook/useResultadoSorteio'
 
 
 const Sorteio = () => {
   const participantes = useListaParticipantes()
+  const [participanteDaVez, setParticipanteDaVez] = useState('')
+  const [amigoSecreto, setAmigoSecreto] = useState('')
+  
+  const resultado = useResultadoSorteio()
+
+
+  const sortear = (event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault()
+          if(resultado.has(participanteDaVez)) {
+            setAmigoSecreto(resultado.get(participanteDaVez)!)
+          }
+  }
   return (
     <> 
   <Header />
+  <form onSubmit={sortear}>
     <section className={style.card}>
-      <select name='ParticipanteDaVez' id='ParticipanteDaVez'>
+      <select
+       required
+       name='ParticipanteDaVez'
+       id='ParticipanteDaVez'
+       placeholder='selecione o seu nome'
+       value={participanteDaVez}
+       onChange={event => setParticipanteDaVez(event.target.value)}
+
+      >
           {participantes.map(participante => (
             <option key={participante}>
               {participante}
             </option>
           ))}
     </select>
+      <button>Sortear</button>
   </section>
-          
-
+</form>
+      {amigoSecreto && <p role='alert'>{amigoSecreto}</p>}
   </>
   )
 }
